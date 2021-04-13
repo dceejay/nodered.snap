f50aad334681f22856aec12c06435d27af2b78cf
Dave Conway-Jones <conway@uk.ibm.com>
2021-04-13 14:27:12 +0100

    change snap to use SNAP_DATA
    to close #21

--- snap/local/settings/start.sh
+++ snap/local/settings/start.sh
@@ -2,9 +2,14 @@
 set -e
 if [ -z "$PORT" ]; then PORT=1880; fi
 export PORT
-if [ ! -f $SNAP_USER_DATA/settings.js ]; then
-    echo "NEW - Copy settings.js to "$SNAP_USER_DATA
-    cp $SNAP/lib/node_modules/node-red/settings.js $SNAP_USER_DATA/
+if [ ! -f $SNAP_DATA/settings.js ]; then
+    if [ -f $SNAP_USER_DATA/settings.js ]; then
+        echo "MIGRATE - Move settings.js to "$SNAP_DATA
+        mv $SNAP_USER_DATA/* $SNAP_DATA/
+    else
+        echo "NEW - Copy settings.js to "$SNAP_DATA
+        cp $SNAP/lib/node_modules/node-red/settings.js $SNAP_DATA/
+    fi
 fi
-echo NOW STARTING NODE-RED : $SNAP/bin/node-red -u $SNAP_USER_DATA on port $PORT
-"$SNAP"/bin/node "$SNAP"/lib/node_modules/.bin/node-red -u "$SNAP_USER_DATA" "$SNAP_USER_DATA/flows.json"
+echo NOW STARTING NODE-RED : $SNAP/bin/node-red -u $SNAP_DATA on port $PORT
+"$SNAP"/bin/node "$SNAP"/lib/node_modules/.bin/node-red -u "$SNAP_DATA" "$SNAP_DATA/flows.json"
